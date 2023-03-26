1cfa1681aba86b39e277f6db81efc01eee415ebe
Louis Venter <venter.leoh@gmail.com>
2023-03-26 09:19:17 +0200

    Added some code for generation of subs

--- src/implicit_enumeration.py
+++ src/implicit_enumeration.py
@@ -1,11 +1,29 @@
 model = []
+available_subs = []
+sub_counter = 0
 
 # Main function to be called by other functions. Initialises global model.
 def implicitEnumeration(arr):
     global model
+    global available_subs
     model = arr
     printModel()
-    print(getMaster())
+
+    available_subs.append(initMaster())
+    print(available_subs)
+    while len(available_subs) > 0:
+        max_z = available_subs[0][1]
+        max_index = 0
+        for i in range(len(available_subs)):
+            if available_subs[i][1] > max_z:
+                max_z = available_subs[i][1]
+                max_index = i
+        left_sub = getLeftSub(available_subs[max_index])
+        right_sub = getRightSub(available_subs[max_index])
+        if len(left_sub) > 0:
+            available_subs.append(left_sub)
+        if len(right_sub) > 0:
+            available_subs.append(right_sub)
 
 # Returns:
 # - <-1> if error
@@ -15,23 +33,18 @@ def implicitEnumeration(arr):
 def isFeasible(arr):
     global model
     answer = 2
-
     for i in range(len(model) - 1):
         t = 0
         z = 0
-        
         for j in range(len(arr)):
             t += arr[j] * model[i + 1][j]
             z = model[i + 1][j + 1]
-        
         if t > z:
             if answer == 2:
                 answer = 0
         else:
             if answer == 0:
                 answer = 1
-
-    # No answer.
     return answer
 
 # Prints model
@@ -41,14 +54,37 @@ def printModel():
     for row in model:
         print(row)
 
-def getMaster():
+# Gets master solution in form
+# [<index>, <z>, [<values locked?>], [<binary values>]]. Call this function only
+# once before any subs are calculated.
+def initMaster():
     global model
+    global sub_counter
     master = []
-
+    binary = []
+    locks = []
+    z = 0
     for i in range(len(model[0])):
         if model[0][i] < 0:
-            master.append(-1)
+            binary.append(0)
+            locks.append(0)
         else:
-            master.append(1)
-
+            binary.append(1)
+            locks.append(0)
+            z += model[0][i]
+    master.append(sub_counter)
+    sub_counter += 1
+    master.append(z)
+    master.append(locks)
+    master.append(binary)
     return master
+
+# Gets the left sub of given solution in the form
+# [<index>, <z>, [<values locked?>], [<binary values>]]
+def getLeftSub(arr):
+    return arr
+
+# Gets the right sub of given solution in the form
+# [<index>, <z>, [<values locked?>], [<binary values>]]
+def getRightSub(arr):
+    return arr
