885dc4b8591bbca725b1a129200e4c29a28aa200
Louis Venter <venter.leoh@gmail.com>
2023-04-01 16:20:09 +0200

    Added skeleton for the calculation of subs

--- src/implicit_enumeration.py
+++ src/implicit_enumeration.py
@@ -1,5 +1,11 @@
+# The original model for problem.
 model = []
+# Array of all subs that have been calculated so far.
+all_subs = []
+# Array of subs in form; [<index>, <z>, [<values locked?>], [<binary values>]],
+# that can be iterated on.
 available_subs = []
+# Current number of sub answers.
 sub_counter = 0
 
 # Main function to be called by other functions. Initialises global model.
@@ -7,22 +13,31 @@ def implicitEnumeration(arr):
     global model
     global available_subs
     model = arr
+    print("MODEL:")
     printModel()
-
-    available_subs.append(initMaster())
-    print(available_subs)
-    while len(available_subs) > 0:
+    print("\n----------------------------------------------------------------\n")
+    master = getMaster()
+    all_subs.append(master)
+    available_subs.append(master)
+    iteration = 0; # TODO temp
+    while len(available_subs) > 0 and iteration < 5:
+        iteration += 1
         max_z = available_subs[0][1]
         max_index = 0
+        # Finds the the sub with the maximum z value.
         for i in range(len(available_subs)):
             if available_subs[i][1] > max_z:
                 max_z = available_subs[i][1]
                 max_index = i
-        left_sub = getLeftSub(available_subs[max_index])
-        right_sub = getRightSub(available_subs[max_index])
+        # Gets the left and right sub of sub.
+        left_sub = getSub(available_subs[max_index], 0)
+        right_sub = getSub(available_subs[max_index], 1)
+        # Adds those subs to list of available subs.
         if len(left_sub) > 0:
+            all_subs.append(left_sub)
             available_subs.append(left_sub)
         if len(right_sub) > 0:
+            all_subs.append(right_sub)
             available_subs.append(right_sub)
 
 # Returns:
@@ -55,36 +70,53 @@ def printModel():
         print(row)
 
 # Gets master solution in form
-# [<index>, <z>, [<values locked?>], [<binary values>]]. Call this function only
+# [<index>, <z>, <values locked>, [<binary values>]]. Call this function only
 # once before any subs are calculated.
-def initMaster():
+def getMaster():
     global model
     global sub_counter
     master = []
     binary = []
-    locks = []
     z = 0
     for i in range(len(model[0])):
         if model[0][i] < 0:
             binary.append(0)
-            locks.append(0)
         else:
             binary.append(1)
-            locks.append(0)
             z += model[0][i]
     master.append(sub_counter)
     sub_counter += 1
     master.append(z)
-    master.append(locks)
+    master.append(0)
     master.append(binary)
     return master
 
-# Gets the left sub of given solution in the form
-# [<index>, <z>, [<values locked?>], [<binary values>]]
-def getLeftSub(arr):
-    return arr
-
-# Gets the right sub of given solution in the form
-# [<index>, <z>, [<values locked?>], [<binary values>]]
-def getRightSub(arr):
-    return arr
+# Gets the left (side = 0) or right (side = 1) sub of given solution in the form
+# [<index>, <z>, <values locked>, [<binary values>]]
+def getSub(arr, side):
+    # TODO do this functions
+    sub = []
+    add_sub = False
+    feasible = isFeasible(arr[3])
+    # Not a feasible solution and no solution after this solution.
+    if feasible == -1:
+        print("Error")
+        # TODO stop program
+    # Not a feasible solution and no solution after this solution.
+    if feasible == 0:
+        # TODO write to output but keep sub empty
+        print("feasible: 0")
+        add_sub = True
+    # Not a candidate soltution but could contain a candidate solution after
+    # this solution.
+    elif feasible == 1:
+        # TODO
+        print("feasible: 1")
+        add_sub = True
+    # Candidate solution.
+    elif feasible == 2:
+        # TODO
+        print("feasible: 2")
+        add_sub = True
+    # TODO add sub to all_subs if necessary
+    return sub
--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from reader import *
 from implicit_enumeration import *
 
 # TODO: make it so file can be set
-file_name = "../data/input1.txt"
+file_name = "data/input1.txt"
 arr = readFromFile(file_name)
 implicitEnumeration(arr)
